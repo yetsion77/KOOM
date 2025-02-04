99d1995adeda3e39fe23c4c85acb349b4681b2bb
yetsion77 <yetsion77@gmail.com>
2025-02-04 20:52:15 +0200

    Fix static files paths

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,22 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/KOOM',
+  assetPrefix: '/KOOM/',
   images: {
     unoptimized: true,
-  }
+  },
+  // נשנה את ההגדרות של webpack
+  webpack: (config) => {
+    config.output = {
+      ...config.output,
+      publicPath: `${process.env.NODE_ENV === 'production' ? '/KOOM' : ''}/`
+    };
+    return config;
+  },
+  // נוסיף הגדרה לשמירת קבצים סטטיים
+  distDir: 'dist',
+  trailingSlash: true
 }
 
 export default nextConfig
